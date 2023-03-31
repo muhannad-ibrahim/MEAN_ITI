f16617473d1af635e1da22cc04c51dd913ac379e
Mohamed Salah <mohamedsalahmasoud173@gmail.com>
2023-03-31 06:29:10 +0200

    adding create book and get book by its id

--- src/controllers/book.js
+++ src/controllers/book.js
@@ -13,9 +13,31 @@ const getAllBooks = async (req, res) => {
     }
 };
 
+const createBook = async (req, res) => {
+    const book = await new Book({
+        name: req.body.name,
+        categoryId: req.body.categoryId,
+        AuthorId: req.body.AuthorId,
+    });
+    book.save().then((savedBook) => {
+        res.json({ message: 'success', savedBook });
+    }).catch((error) => {
+        res.json({ message: 'error', error });
+    });
+};
+
+const getBookById = async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id);
+        res.json({ message: 'success', book });
+    } catch (error) {
+        res.json(error.message);
+    }
+};
 module.exports = {
+    createBook,
     getAllBooks,
-    // getBookById,
+    getBookById,
     // updateBookById,
     // deleteBookById,
 };
--- src/routes/books.js
+++ src/routes/books.js
@@ -4,7 +4,8 @@ const { bookController } = require('../controllers');
 /* eslint-disable comma-dangle */
 
 router.get('/', bookController.getAllBooks);
-// router.get('/:id', bookController.getBookById);
+router.post('/', bookController.createBook);
+router.get('/:id', bookController.getBookById);
 // router.patch('/:id', bookController.updateBookById);
 // router.delete('/:id', bookController.deleteBookById);
 
