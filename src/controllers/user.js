35acb773e525f3bbafeeddad15b9f2ce0da28634
Mohamed Salah <mohamedsalahmasoud173@gmail.com>
2023-04-10 15:47:12 +0200

    modify user controller to using pagination package

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,3 +1,5 @@
+/* eslint-disable radix */
+/* eslint-disable consistent-return */
 // eslint-disable-next-line no-unused-vars
 const { query } = require('express');
 const fs = require('fs');
@@ -8,10 +10,7 @@ const checkRole = require('../middleware/checkRole');
 const JWT_SECRET = process.env.JWT_SECRET || 'test';
 
 const signup = async (req, res) => {
-    let imageURL;
-    if (req.file) {
-        imageURL = `${req.protocol}://${req.headers.host}/${req.file.filename}`;
-    }
+    const imageURL = `${req.protocol}://${req.headers.host}/${req.file.filename}`;
     const user = new User({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
@@ -33,16 +32,22 @@ const getAllUsers = async (req, res) => {
     if (!checkRole.isAdmin(req)) {
         res.json({ message: 'error', error: 'You are not an admin' });
     }
+    const itemPerPage = 5;
+    const currentPage = parseInt(req.query.page) || 1;
     try {
-        const pageNumber = parseInt(req.query.pageNumber, 10) || 0;
-        const pageSize = parseInt(req.query.pageSize, 10) || 6;
-        const users = await User
-            .find()
-            .skip((pageNumber) * pageSize)
-            .limit(pageSize)
-            .exec();
-        const usersCount = await User.countDocuments();
-        res.json({ data: users, total: usersCount });
+        const users = await User.paginate({}, { page: currentPage, limit: itemPerPage });
+        if (users.docs.length === 0) {
+            return res.status(404).json({ message: 'there is no users' });
+        }
+        res.json({
+            message: 'success',
+            data: users.docs,
+            pages: users.totalPages,
+            currentPage: users.page,
+            nextPage: users.hasNextPage ? users.nextPage : null,
+            prevPage: users.hasPrevPage ? users.prevPage : null,
+
+        });
     } catch (error) {
         res.json(error.message);
     }
@@ -60,56 +65,7 @@ const getUserById = async (req, res) => {
     }
 };
 
-const getUserBooks = async (req, res) => {
-    try {
-        const pageNumber = parseInt(req.query.pageNumber, 10) || 0;
-        const pageSize = parseInt(req.query.pageSize, 10) || 6;
-        const token = req.cookies.jwt;
-        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-        const users = await User
-            .findById(decodedToken.id)
-            .populate({
-                path: 'books.bookId',
-                select: 'name AuthorId photo rating',
-                populate: {
-                    path: 'AuthorId',
-                    select: 'firstName',
-                },
-            })
-            .skip((pageNumber) * pageSize)
-            .limit(pageSize)
-            .exec();
-        const usersCount = await User.countDocuments();
-        res.json({ data: users, total: usersCount });
-    } catch (error) {
-        res.json(error.message);
-    }
-};
-
-const addBookToUser = async (req, res) => {
-    try {
-        // Get the book ID from the URL parameter
-        const bookId = req.params.id;
-
-        // Get the JWT from the cookies and decode it to get the user ID
-        const token = req.cookies.jwt;
-        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-
-        // Find the user in the database
-        const user = await User.findById(decodedToken.id);
-
-        // Add the book to the user's array of books
-        user.books.push({ bookId });
-        // Save the user's changes
-        await user.save();
-        res.status(200).json({ message: 'Book added to user successfully' });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error adding book to user' });
-    }
-};
-
-async function updateUserById(req, res) {
+const updateUserById = async (req, res) => {
     if (!checkRole.isAdmin(req)) {
         res.json({ message: 'error', error: 'You are not an admin' });
     }
@@ -126,7 +82,7 @@ async function updateUserById(req, res) {
     } catch (error) {
         res.json(error.message);
     }
-}
+};
 
 const deleteUserById = async (req, res) => {
     if (!checkRole.isAdmin(req)) {
@@ -151,17 +107,14 @@ const deleteUserById = async (req, res) => {
 const login = async (req, res) => {
     const { body: { email, password } } = req;
     const user = await User.findOne({ email }).exec();
-    if (!user) {
-        return res.json({ message: 'error', error: 'user does\'t exist' });
-    }
     const valid = user.verifyPassword(password);
     if (!valid) {
-        return res.json({ message: 'error', error: 'UNAUTHENTICATED to login' });
+        res.json({ message: 'error', error: 'UNAUTHENTICATED to login' });
     }
     const token = jwt.sign({ email, id: user.id, role: user.role }, JWT_SECRET, { expiresIn: '4h' });
     res.cookie('jwt', token, { httpOnly: true, maxAge: 1000 * 60 * 60 * 4 });
 
-    return res.json({ message: 'success' });
+    res.json({ message: 'success' });
 };
 
 const getUserProfile = async (req, res) => {
@@ -202,6 +155,4 @@ module.exports = {
     getUserProfile,
     login,
     logout,
-    getUserBooks,
-    addBookToUser,
 };
