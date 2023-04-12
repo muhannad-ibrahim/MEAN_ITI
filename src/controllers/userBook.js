b4a287538e9224fe752749da04beffec08b9ad09
muhannad ibrahim <muhannad.ibrahim2311@gmail.com>
2023-04-12 23:46:30 +0200

    add asyncWrapper on userBook controller

--- src/controllers/userBook.js
+++ src/controllers/userBook.js
@@ -4,72 +4,116 @@
 /* eslint-disable no-unused-vars */
 const UserBook = require('../models/userBooks');
 
-const create = (req, res) => {
+const create = async (req, res, next) => {
+    const userId = req.body.UserId;
+    const { bookId } = req.body;
+
+    const userPromise = User.findById(userId).exec();
+    const [userErr, user] = await asyncWrapper(userPromise);
+    if (userErr) {
+        return next(userErr);
+    }
+
+    if (!user) {
+        return res.status(404).send({ message: 'User not found' });
+    }
+
+    const bookPromise = Book.findById(bookId).exec();
+    const [bookErr, book] = await asyncWrapper(bookPromise);
+    if (bookErr) {
+        return next(bookErr);
+    }
+
+    if (!book) {
+        return res.status(404).send({ message: 'Book not found' });
+    }
+
     const userBook = new UserBook({
-        UserId: '642624b835bf073262dc5d9b',
-        // UserId: 1,
-        bookId: '642624ba35bf073262dc5da4',
-        // bookId: 21,
+        UserId: userId,
+        bookId,
         reviews: [
             { reviewerName: 'user1', comment: 'Great book!', rate: 5 },
             { reviewerName: 'user2', comment: 'I enjoyed it.', rate: 4 },
         ],
     });
-    userBook.save()
-        .then(() => res.json({ message: 'success' }))
-        .catch((error) => res.json({ message: error.message }));
+
+    const savePromise = userBook.save();
+    const [saveErr] = await asyncWrapper(savePromise);
+    if (saveErr) {
+        return next(saveErr);
+    }
+
+    return res.json({ message: 'success' });
 };
 
-const getUserBooks = async (req, res) => {
+const getUserBooks = async (req, res, next) => {
     const currentPage = parseInt(req.query.page) || 1;
     const itemPerPage = 5;
+    const token = req.cookies.jwt;
+    let decodedToken;
     try {
-        const token = req.cookies.jwt;
-        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-        const users = await UserBook.paginate({}, { page: currentPage, limit: itemPerPage })
-            .findById(decodedToken.id)
-            .populate({
-                path: 'books.bookId',
-                select: 'name AuthorId photo rating',
-                populate: {
-                    path: 'AuthorId',
-                    select: 'firstName',
-                },
-            });
-        return res.json({
-            message: 'success',
-            data: users.docs,
-            pages: users.totalPages,
-            currentPage: users.page,
-            nextPage: users.hasNextPage ? users.nextPage : null,
-            prevPage: users.hasPrevPage ? users.prevPage : null,
-        });
-    } catch (error) {
-        res.json(error.message);
+        decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (err) {
+        return next({ message: 'Unauthenticated' });
     }
+
+    const promise = UserBook
+        .find({ UserId: decodedToken.id })
+        .populate({
+            path: 'bookId',
+            select: 'name AuthorId photo rating',
+            populate: {
+                path: 'AuthorId',
+                select: 'firstName',
+            },
+        })
+        .skip((currentPage - 1) * itemPerPage)
+        .limit(itemPerPage)
+        .exec();
+
+    const [err, userBooks] = await asyncWrapper(promise);
+    if (err) {
+        return next(err);
+    }
+
+    const countPromise = UserBook.countDocuments({ UserId: decodedToken.id }).exec();
+    const [countErr, count] = await asyncWrapper(countPromise);
+    if (countErr) {
+        return next(countErr);
+    }
+
+    const pages = Math.ceil(count / itemPerPage);
+    const nextPage = currentPage < pages ? currentPage + 1 : null;
+    const prevPage = currentPage > 1 ? currentPage - 1 : null;
+
+    return res.json({
+        message: 'success',
+        data: userBooks,
+        pages,
+        currentPage,
+        nextPage,
+        prevPage,
+    });
 };
 
-const addBookToUser = async (req, res) => {
-    try {
-        // Get the book ID from the URL parameter
-        const bookId = req.params.id;
-
-        // Get the JWT from the cookies and decode it to get the user ID
-        const token = req.cookies.jwt;
-        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-
-        // Find the user in the database
-        const user = await User.findById(decodedToken.id);
-
-        // Add the book to the user's array of books
-        user.books.push({ bookId });
-        // Save the user's changes
-        await user.save();
-        res.status(200).json({ message: 'Book added to user successfully' });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error adding book to user' });
+const addBookToUser = async (req, res, next) => {
+    const bookId = req.params.id;
+    const token = req.cookies.jwt;
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    const userPromise = User.findById(decodedToken.id);
+    const [userErr, user] = await asyncWrapper(userPromise);
+    if (userErr) {
+        return next({ message: 'Error finding user' });
     }
+
+    user.books.push({ bookId });
+    const saveUserPromise = user.save();
+    const [saveErr] = await asyncWrapper(saveUserPromise);
+    if (saveErr) {
+        return next({ message: 'Error saving user while adding book' });
+    }
+
+    res.status(200).json({ message: 'Book added to user successfully' });
 };
 
 module.exports = {
